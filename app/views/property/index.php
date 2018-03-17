9b108f09912a0f1662cdb85090f3a4a23bb25151
Shubham Chawla <shubham_chawla@live.com>
2018-03-17 20:46:34 +0530

    Created Properties View

--- app/views/property/index.php
+++ app/views/property/index.php
@@ -1,5 +1,7 @@
 <?php require APPROOT.'/views/inc/header.php'; ?>
 
-Property Index Page comes here
+<!-- Properties -->
+<h1 style="border-bottom-style: solid; border-bottom-width: 1px; border-bottom-color: #262626;">Properties</h1>
+<p>Fetch List of properties and their current prices & their % to users</p>
 
 <?php require APPROOT.'/views/inc/footer.php'; ?>
